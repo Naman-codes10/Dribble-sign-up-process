564afb505677da2ba6ef3f49d349895952b55e1f
Naman <116005558+Naman-codes10@users.noreply.github.com>
2024-04-09 11:09:18 +0530

    .

--- javascript/cards.js
+++
@@ -1,9 +0,0 @@
-const options = document.querySelectorAll('.option');
-const checkboxes = document.querySelectorAll('input[name="option"]');
-
-options.forEach((option, index) => {
-  option.addEventListener('click', () => {
-    option.classList.toggle('selected');
-    checkboxes[index].checked = !checkboxes[index].checked;
-  });
-});
